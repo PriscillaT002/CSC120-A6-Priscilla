6b25a90b474ec743e33112996e0b5b29ecb68d25
Priscilla Trejo <112957372+PriscillaT002@users.noreply.github.com>
2022-11-06 01:45:16 -0500

    new

--- Cafe.java
+++ Cafe.java
@@ -1,22 +1,50 @@
 /* This is a stub for the Cafe class */
+
 public class Cafe extends Building{
+    public int nCoffeeOunces;
+    public int nSugarPackets;
+    public int nCreams;
+    public int nCups;
     
     
     public Cafe(String name, String address, int nFloors, int coffee, int sugar, int cream, int cups) {
         super(name, address, nFloors);
+        coffee = 50;
+        sugar = 50;
+        cream = 50;
+        cups = 50;
         this.nCoffeeOunces = coffee;
-        this.nSugarPackets= sugar;
-        this.nCreams= cream;
-        this.nCups= cups;
+        this.nSugarPackets = sugar;
+        this.nCreams = cream;
+        this.nCups = cups;
+
 
         System.out.println("You have built a cafe: ☕");
     }
 
-    public void sellCoffee(int size, int nSugarPackets, int nCreams) {
+    public void sellCoffee(int pCoffee, int pSugar, int pCream) {
+
+        nCoffeeOunces -= pCoffee;
+        nSugarPackets -= pSugar;
+        nCreams -= pCream;
 
     }
 
-    private void restock(int nCoffeeOunces, int nSugarPackets, int nCreams, int nCups) {
+        private void restock(int coffee, int sugar, int cream) {
+            if (coffee == 0) {
+                nCoffeeOunces += 50;}
+
+            if (sugar == 0) {
+                nSugarPackets += 50;}
+
+            if  (cream == 0) {
+                nCreams += 50 ;}
+
+              }
+             
+            
+            }
+
 
     }
     
--- Library.java
+++ Library.java
@@ -1,46 +1,96 @@
+
 /* This is a stub for the Library class */
 import java.util.Hashtable
 
 public class Library extends Building{
+
+  Hashtable<String, Boolean> newLibrary;
+
+  public Library(String name, String address, int nFloors, String title, Boolean have) {
+    super(name,address,nFloors);
+
+    Hashtable<String, Boolean> newLibrary = new Hashtable<String, Boolean>();
   
-  public static void table(String args[])
-  {
-      // No need to mention the
-      // Generic type twice
-      Hashtable<Integer, String> ht1 = new Hashtable<>();
 
-      // Initialization of a Hashtable
-      // using Generics
-      Hashtable<Integer, String> ht2
-          = new Hashtable<Integer, String>();
+    System.out.println("You have built a library: 📖");
 
-      // Inserting the Elements
-      // using put() method
-      ht1.put(1, "one");
-      ht1.put(2, "two");
-      ht1.put(3, "three");
 
-      ht2.put(4, "four");
-      ht2.put(5, "five");
-      ht2.put(6, "six");
-
-      // Print mappings to the console
-      System.out.println("Mappings of ht1 : " + ht1);
-      System.out.println("Mappings of ht2 : " + ht2);
   }
+
+  public void addTitle(String title, Boolean have) {
+
+    newLibrary.put(title, have);
+
+
+  }
+
+  public String removeTitle(String title, Boolean have){
+
+    newLibrary.remove(title, have);
+
+    return title;
+  }
+
+  public void checkOut(String title, Boolean have){
+
+   newLibrary.remove(title, have);
+
+
+    if (have == true) {
+      System.out.println(newLibrary.get(have) + "This book is currently checked out"); }
+
+
+
+  }
+
+  public void returnBook(String title, Boolean have) {
+
+    newLibrary.put(title, have);
+
+  if (have == true) {
+    System.out.println(newLibrary.get(have) + "This book is currently available"); }
+
+  }
+
+  // returns true if the title appears as a key in the Libary's collection, false otherwise
+  public boolean containsTitle(String title, Boolean have){
+    if (have == true) {
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  // returns true if the title is currently available, false otherwise
+  public boolean isAvailable(String title, Boolean have){
+    if (have == true) {
+      return true;
+      System.out.println("This title is currently available");
+    }
+    else{
+      return false;
+    }
+
+  }
+
+  // prints out the entire collection in an easy-to-read way (including checkout status)
+  public void printCollection(){
+
+    System.out.println("------------------------------------");
+    System.out.println(newLibrary);
+    System.out.println("------------------------------------");
+
+
+
+
+  }
+
+
+
+
+  public static void main(String[] args) {
+    new Library();
+  }
+
 }
-
-
-
-
-
-    public Library(String name, String address, Int nFloors) {
-      Super 
-      System.out.println("You have built a library: 📖");
-    }
-  
-    public static void main(String[] args) {
-      new Library();
-    }
-  
-  }
